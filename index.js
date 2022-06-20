bd131f8758dcde3b31aa8fb408ceaddbe25023b0
Gabriel Boudreaux <boudreauxgabriel1@gmail.com>
2022-06-20 12:06:50 -0500

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Write your solution here!
+
+const cats =["Milo", "Otis", "Garfield"] 
+
+beforeEach(function () {
+ cats.length = 0;
+
+ cats.push("Milo", "Otis", "Garfield");
+    
+});
+
+function destructivelyAppendCat(name){
+cats.push(name);
+return cats;
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return cats;
+    
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+    return cats;
+}
+  
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+    return cats;
+}
+
+function appendCat(name){
+    const morecats = cats.slice();
+    morecats.push(name);
+    return morecats;
+    
+}
+
+function prependCat(name){
+    const lesscats = [name, ...cats];
+    return lesscats;
+}
+
+function removeLastCat(){
+    const caats = cats.slice(0,cats.length-1);
+    return caats;
+}
+function removeFirstCat(){
+    const caats = cats.slice(1);
+    return caats;
+}
